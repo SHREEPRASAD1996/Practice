cea51ebda1c6387c9d9e1ec72f5b7de5e4f12ab5
Shreeprasad Tripathy <111556063+SHREEPRASAD1996@users.noreply.github.com>
2022-09-26 00:04:23 -0700

    Add files via upload

---
+++ 26.09.2022/Add.java
@@ -0,0 +1,11 @@
+//Java Program to Add two Numbers
+public class Add {
+	public static void main(String[] args)
+	{
+	int a,b,c;
+	a= 1;
+	b=2;
+	c=a+b;
+	System.out.println("Sum of two number is : " + c);
+	}
+}
---
+++ 26.09.2022/Multiplication.java
@@ -0,0 +1,13 @@
+//Java Program to Multiply two Numbers
+
+public class Multiplication
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a= 10;
+b=20;
+c=a*b;
+System.out.println("Multiplication of two number is : " + c);
+}
+}
